2bfc0582789d93345769a832cc33dec5404c3a6b
Yoftahe Gizaw <140615264+Yofgiz@users.noreply.github.com>
2024-02-26 18:52:45 +0300

    Create 2-binary_tree_insert_right.c

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right -insert a node to the right side
+ * @parent: pointer to the node.
+ * @value: value
+ * Return: NULL or pointer to a node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+
+	return (new);
+}
