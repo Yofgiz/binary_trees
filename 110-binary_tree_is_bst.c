106ccde732b1a181d66ec47de937376d9df72651
Yoftahe Gizaw <140615264+Yofgiz@users.noreply.github.com>
2024-02-26 20:11:49 +0300

    Update 110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,1 +1,28 @@
+#include "binary_trees.h"
+#include "limits.h"
 
+/**
+ * is_bst_helper - check the validity of the binary search tree.
+ * @lo: smallest node visited.
+ * @hi: largest node visited.
+ *
+ * Return: if tree is valid,1 or rlse 0.
+ */
+int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
+{
+	if (tree != NULL)
+	{
+		if (tree->n < lo || tree->n > hi)
+			return (0);
+		return (is_bst_helper(tree->left, lo, tree->n - 1) &&
+			is_bst_helper(tree->right, tree->n + 1, hi));
+	}
+	return (1);
+}
+
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (is_bst_helper(tree, INT_MIN, INT_MAX));
+}
