791099ab1a5818427b519879009051956bcd98ce
Yoftahe Gizaw <140615264+Yofgiz@users.noreply.github.com>
2024-02-26 21:45:36 +0300

    Create 100-short_binary_trees_ancestor.c

---
+++ 100-short_binary_trees_ancestor.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - lowest common ancestor of two nodes.
+ * @first: Points to the first node.
+ * @second: Points to the second node.
+ *
+ * Return: return common ancestor or NULL.
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *mom, *pop;
+
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	mom = first->parent, pop = second->parent;
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	else if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
+}
